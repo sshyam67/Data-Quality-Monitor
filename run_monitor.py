04ebd835b8ccb26642e5d378888f21c95f92e938
Shyamkrishna Sreeramasetty <sshyamkrishna67@gmail.com>
2025-06-30 10:32:52 +0530

    Update run_monitor.py

--- run_monitor.py
+++ run_monitor.py
@@ -1,5 +1,1 @@
-from src.data_validator import DataValidator
-
-if __name__ == "__main__":
-    validator = DataValidator("data/sample_data.csv", "config/rules_config.yaml")
-    validator.run_checks()
+#no code given
